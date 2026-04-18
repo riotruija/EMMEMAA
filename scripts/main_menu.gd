289455102e4720e4b4224883bc59b29a93b7b876
Markus Schaffrik <markus.schaffrik@ut.ee>
2026-04-18 11:51:54 +0300

    Veel menu ja muusika

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,20 @@
 extends Control
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+@onready var main_buttons: VBoxContainer = $Main_buttons
+@onready var options: Panel = $Options
+
+var muusika = [
+	preload("res://music/main_menu_music.mp3")
+]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("Options node: ", options)
+	print("Buttons node: ", main_buttons)
+	options.visible = false
+	main_buttons.visible = true
+	# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,12 +23,23 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
+	print("Start pressed")
+	# get_tree().change_scene_to_file("res://scenes/earth.tscn") //Vahetab siis kui valmis
 	pass # Alustab pelu
 
 
 func _on_options_pressed() -> void:
+	print("Options pressed")
+	main_buttons.visible = false
+	options.visible = true
 	pass # Viib optionitesse
 
 
 func _on_exit_pressed() -> void:
-	pass # Väljub
+	print("Exit pressed")
+	get_tree().quit(0) # Väljub
+
+
+func _on_options_back_pressed() -> void:
+	main_buttons.visible = true
+	options.visible = false
