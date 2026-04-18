1c7181f7d03f9ac99296757f4b7680e367f90939
Markus Schaffrik <markus.schaffrik@ut.ee>
2026-04-18 10:32:15 +0300

    Main menu n stuff

---
+++ scenes/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	pass # Alustab pelu
+
+
+func _on_options_pressed() -> void:
+	pass # Viib optionitesse
+
+
+func _on_exit_pressed() -> void:
+	pass # Väljub
