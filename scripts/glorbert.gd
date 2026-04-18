ecf0d023f49e4cb488b07bbbb4a4278c4526f740
riotruija <rio.truija@ut.ee>
2026-04-18 13:45:37 +0300

    glorbert liigub

--- scripts/glorbert.gd
+++ scripts/glorbert.gd
@@ -1,31 +1,54 @@
 extends Node2D
 
 @export var glorbert: Node2D
-var rigidbody_glorbert: RigidBody2D
+var glorbert_keha: CharacterBody2D
 @export var maapind: Node2D
 var maapinna_pind: StaticBody2D
 
-var JUMP_STRENGTH = 300
+var SAAPAD_YLES = 300
+var SAAPAD_EDASITAGASI = 150
+var MAX_KIIRUS_YLES = 400
+var MAX_KIIRUS_EDASITAGASI = 200
+var GRAVITATSIOON = 600
+var HOORDUMINE = 1000
+
+var hoiab_paremale: bool = false
+var hoiab_vasakule: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rigidbody_glorbert = $RigidBody2D
-	rigidbody_glorbert.contact_monitor = true
-	rigidbody_glorbert.max_contacts_reported = 8
+	glorbert_keha = $glorbert_keha
 	maapinna_pind = maapind.get_node("StaticBody2D")
 
-func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action("up") and event.is_pressed():
-
-		var porkujad = rigidbody_glorbert.get_colliding_bodies()
-		print(porkujad)
-		if (porkujad.has(maapinna_pind)):
-			rigidbody_glorbert.apply_impulse(Vector2(0, -JUMP_STRENGTH), Vector2(0, 0))
+func _unhandled_key_input(event: InputEvent) -> void:		
+	if event.is_action("left") and event.is_pressed():
+		hoiab_vasakule = true
 		
+	if event.is_action("left") and event.is_released():
+		hoiab_vasakule = false
 		
-
+	if event.is_action("right") and event.is_pressed():
+		hoiab_paremale = true
+		
+	if event.is_action("right") and event.is_released():
+		hoiab_paremale = false
+	
+func _physics_process(delta: float) -> void:
+	if not glorbert_keha.is_on_floor():
+		glorbert_keha.velocity.y += GRAVITATSIOON * delta
+	
+	if hoiab_paremale and not hoiab_vasakule:
+		glorbert_keha.velocity.x = SAAPAD_EDASITAGASI
+	elif hoiab_vasakule and not hoiab_paremale:
+		glorbert_keha.velocity.x = -SAAPAD_EDASITAGASI
+	else:
+		glorbert_keha.velocity.x = move_toward(glorbert_keha.velocity.x, 0, HOORDUMINE * delta)
+	if Input.is_action_just_pressed("up") and glorbert_keha.is_on_floor():
+		print("kargab")
+		glorbert_keha.velocity.y = -SAAPAD_YLES
+	
+	glorbert_keha.move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	pass
