593e49fdeb368bdbe6f0bb8adfb069c93d21122c
markus <markus>
2026-04-19 15:18:49 +0300

    End screen and big wave

--- scripts/suurlaine.gd
+++ scripts/suurlaine.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var KIIRUS = 100
+@onready var sound_player: AudioStreamPlayer2D = $Sound_player
 
 func _physics_process(delta: float):
 	position.x -= KIIRUS * delta
@@ -11,11 +12,13 @@ func _physics_process(delta: float):
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("tekkis suur laine")
+	sound_player.play()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	sound_player.pitch_scale = randf_range(1.0, 1.6)
 	pass
 
 
